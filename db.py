4e8c6d5f499b24090fab3ace27a5c514f0aaf6bc
prowayne <prosmarter@163.com>
2023-11-28 01:27:41 +0800

    profile db query

--- db.py
+++ db.py
@@ -2957,7 +2957,7 @@ class Database:
             (2, self.migrate_2_add_helper_functions),
             (3, self.migrate_3_set_mapping_history_key_not_null),
             (4, self.migrate_4_support_batch_certificate_v2),
-            (5, self.migrate_5_fix_missing_program),
+            # (5, self.migrate_5_fix_missing_program),
         ]
         async with self.pool.connection() as conn:
             async with conn.cursor() as cur:
